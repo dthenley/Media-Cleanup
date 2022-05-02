35f1a59a0149e2f583a1ddaf50aa815bc3b224e2
Donte Henley <donte@dontehenley.com>
2022-05-01 20:44:45 -0500

    Upload template from Wordpress Boilerpolate plugin

---
+++ includes/class-media-cleanup-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://dontehenley.com/
+ * @since      1.0.0
+ *
+ * @package    Media_Cleanup
+ * @subpackage Media_Cleanup/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Media_Cleanup
+ * @subpackage Media_Cleanup/includes
+ * @author     Donte Henley <donte@dontehenley.com>
+ */
+class Media_Cleanup_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
